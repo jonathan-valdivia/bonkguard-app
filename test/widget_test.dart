acbc5a68ea05e223f03d28333b88088eb00e9956
Jonathan Valdivia <38237313+jonathan-valdivia@users.noreply.github.com>
2025-11-30 22:26:38 -0700

    commented out the unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bonkguard_app/main.dart';
+// UNCOMMENT at a future date - I commented this out when the tests were failing
+// with the default tests. When I removed those tests and added one test that
+// only checks to see if the app is running this was not needed. Leaving here so
+// I can uncomment when needed later.
+// import 'package:bonkguard_app/main.dart';
 
 void main() {
   testWidgets('App builds without crashing', (WidgetTester tester) async {
